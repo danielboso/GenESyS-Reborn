237e6bcb6d9504451923453127201701f018bc14
Daniel Boso <bosodaniel@hotmail.com>
2018-09-15 11:36:08 -0300

    Update CollectorDatafileDanielBoso_if.h

--- CollectorDatafileDanielBoso_if.h
+++ CollectorDatafileDanielBoso_if.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   CollectorDatafileMyImpl1.h
- * Author: cancian
- *
- * Created on 30 de Agosto de 2018, 16:53
- */
-
 #ifndef COLLECTORDATAFILEDANIELBOSO_H
 #define COLLECTORDATAFILEDANIELBOSO_H
 
