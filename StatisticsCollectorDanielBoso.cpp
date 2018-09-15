bf83006b0ea8b2fde76b85064817e7ad7287dabc
danielboso <bosodaniel@hotmail.com>
2018-09-15 16:08:55 -0300

    Statistics Collector .cpp

---
+++ StatisticsCollectorDanielBoso.cpp
@@ -0,0 +1,25 @@
+
+#include "StatisticsCollectorDanielBoso.h"
+
+StatisticsCollectorDanielBoso::StatisticsCollectorDanielBoso() : ModelInfrastructure(typeid (this).name()) {}
+
+StatisticsCollectorDanielBoso::StatisticsCollectorDanielBoso(std::string name) : ModelInfrastructure(typeid (this).name()) {
+	_name = name;
+}
+
+StatisticsCollectorDanielBoso::StatisticsCollectorDanielBoso(const StatisticsCollectorDanielBoso& orig) : ModelInfrastructure(orig) {}
+
+StatisticsCollectorDanielBoso::~StatisticsCollectorDanielBoso() {}
+
+std::string StatisticsCollectorDanielBoso::show() {
+	return ModelInfrastructure::show();
+}
+
+void StatisticsCollectorDanielBoso::_loadInstance(std::list<std::string> words) {}
+
+std::list<std::string>* StatisticsCollectorDanielBoso::_saveInstance() {
+	std::list<std::string>* words = new std::list<std::string>();
+	return words;
+}
+
+bool StatisticsCollectorDanielBoso::_verifySymbols(std::string* errorMessage) {}
